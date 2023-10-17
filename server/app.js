fbc8c7240255654cebb71606221f35898d84ab45
Akash <akashdey9992@gmail.com>
2023-10-17 20:48:16 +0530

    app modified

--- server/app.js
+++ server/app.js
@@ -1,17 +1,11 @@
 import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
-import fs from "fs";
-import path from "path";
 import userRoutes from "./routes/userRoutes.js";
 import transferRoutes from "./routes/transferRoutes.js";
 import connectToMongoDB from "./config/dbConnect.js";
 
 dotenv.config();
-
-const certificateFile = fs.readFileSync(
-  "./B7399AA2CF709F07271A319D8612F6ED.txt"
-);
 
 console.log();
 
@@ -29,13 +23,6 @@ app.use("/api/v1/bank", transferRoutes);
 app.get("/", (req, res) => {
   res.json("Hello From express 😎");
 });
-
-app.get(
-  "/.well-known/pki-validation/B7399AA2CF709F07271A319D8612F6ED.txt",
-  (req, res) => {
-    res.sendFile(path.resolve("./B7399AA2CF709F07271A319D8612F6ED.txt"));
-  }
-);
 
 app.listen(PORT, () => {
   console.log(`Server running at port ${PORT}`);
